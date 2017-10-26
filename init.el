9d7945baa7f92eb0ada8d7d798fe05ca02639c6b
haoshijie <haoshijie@danale.com>
2017-10-26 09:56:45 +0800

    增加org-mouse操作

--- init.el
+++ init.el
@@ -110,7 +110,11 @@
 (global-set-key (kbd "C-a") 'back-to-indentation)
 (global-set-key (kbd "C-x C-a") 'move-beginning-of-line)
 ;; 自动缩进
- (auto-indent-global-mode)
+(auto-indent-global-mode)
+
+;; 引入org mouse
+(require 'org-mouse)
+
 ;; 设置org-mode自动换行
 (add-hook 'org-mode-hook (lambda()(setq truncate-lines nil)))
 
@@ -164,13 +168,11 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
+ '(org-agenda-files
+   (quote
+    ("~/org/note/ssl.org" "/home/danale/org/agenda/ann.org" "/home/danale/org/agenda/finish.org" "/home/danale/org/agenda/inbox.org" "/home/danale/org/agenda/plan.org")))
  '(speedbar-default-position (quote left))
  '(sr-speedbar-auto-refresh nil)
  '(sr-speedbar-right-side nil)
  '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
